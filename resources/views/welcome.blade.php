99799054b6fa8d50e07b1b3c57bd467c43bd698b
amrMnabih <amr.nabih2003@gmail.com>
2024-05-04 20:28:56 +0300

    project run without menu error

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -120,7 +120,7 @@
                 TODAY'S SPECIALITY</h2>
         </div>
         <div class="container w-full px-5 py-6 mx-auto">
-            <div class="grid lg:grid-cols-4 gap-y-6">
+            {{-- <div class="grid lg:grid-cols-4 gap-y-6">
                 @foreach ($specials->menus as $menu)
                     <div class="max-w-xs mx-4 mb-2 rounded-lg shadow-lg">
                         <img class="w-full h-48" src="{{ Storage::url($menu->image) }}" alt="Image" />
@@ -134,7 +134,7 @@
                         </div>
                     </div>
                 @endforeach
-            </div>
+            </div> --}}
         </div>
     </section>
     <section class="pt-4 pb-12 bg-gray-800">
